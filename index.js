609ec744c1b63d17385660091ea26b12f6f020af
Kunal Shah <kunalshah1996@gmail.com>
2022-11-06 22:57:48 -0600

    Added cors for frontend

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "https://app-buddy.netlify.app/", credentials: true }));
 app.use(
     session({
         secret: "KunalSamruddhi",
