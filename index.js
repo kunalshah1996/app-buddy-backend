73be3414ae585afe541eeed1978604122d51677a
Kunal Shah <kunalshah1996@gmail.com>
2022-11-07 23:19:13 -0600

    Cors

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 
 
 app.use(express.json());
-app.use(cors({ origin: "https://app-buddy.netlify.app/", credentials: true }));
+app.use(cors({ origin: "https://app-buddy.netlify.app", credentials: true }));
 app.use(
     session({
         secret: "KunalSamruddhi",
