1d6c7d21a261f44a6027370d121dc1e0d07cae9b
Kunal Shah <kunalshah1996@gmail.com>
2022-12-01 14:14:14 -0600

    Prod changes

--- index.js
+++ index.js
@@ -14,38 +14,24 @@ import mailRoutes from "./routes/mail.js";
 dotenv.config();
 
 const app = express();
-// app.disable("X-Powered-By");
-
-// app.set("trust proxy", 1); // -------------- FIRST CHANGE ----------------
-
-// app.use(cors({ origin: process.env.ORIGIN, credentials: true, methods: "GET, POST, PUT, DELETE" }));
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Credentials", true);
-//   res.header("Access-Control-Allow-Origin", process.env.ORIGIN);
-//   res.header("Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-HTTP-Method-Override, Set-Cookie, Cookie");
-//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-//   next();
-// });
-
-
-
-
-//Uncomment these changes for production
-
 app.disable("X-Powered-By");
 
 app.set("trust proxy", 1); // -------------- FIRST CHANGE ----------------
 
-app.use(cors({ origin: "https://app-buddy.netlify.app", credentials: true, methods: "GET, POST, PUT, DELETE" }));
+app.use(cors({ origin: process.env.ORIGIN, credentials: true, methods: "GET, POST, PUT, DELETE" }));
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Origin", "https://app-buddy.netlify.app");
+  res.header("Access-Control-Allow-Origin", process.env.ORIGIN);
   res.header("Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-HTTP-Method-Override, Set-Cookie, Cookie");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   next();
 });
+
+
+
+
+//Uncomment these changes for production
 
 app.use(express.json());
 app.use(cors({ origin: process.env.ORIGIN, credentials: true })); //"https://app-buddy.netlify.app"
